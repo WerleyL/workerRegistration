f16de607042fc2bbc03d451d61d7abedc6a9c9da
WerleyL <werleyluciog@gmail.com>
2024-03-12 08:11:52 -0300

    primeira parte

---
+++ src/entities/Departament.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Departament{
+
+
+        private String name;
+        //constructor
+        public Departament(){}
+
+        public Departament(String name) {
+            this.name = name;
+        }
+
+// getter and setter
+    public String getName() {
+       return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
